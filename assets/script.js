8d7e67949aee46789be6d810ecddd4e48c735a88
Kevin-tre <kevintrepied@gmail.com>
2024-04-29 18:53:03 +0200

    add function createBulletPoints

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,43 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+// EVENT LISTENER SUR LES FLECHES //
+
+let arrowLeft = document.querySelector(".arrow_left")
+
+arrowLeft.addEventListener("click", () => {
+	console.log("Clique sur la flèche gauche");
+})
+
+let arrowRight = document.querySelector(".arrow_right")
+
+arrowRight.addEventListener("click", () => {
+	console.log("Clique sur la flèche droite");
+})
+
+
+// Ajout des bullet points //
+
+// const bulletPoints = `
+// 	<div class="dot">.</div>
+// 	<div class="dot">.</div>
+// 	<div class="dot">.</div>
+// 	<div class="dot">.</div>
+// `
+
+// document.querySelector(".dots").innerHTML = bulletPoints;
+
+function createBulletPoints () {
+
+	for (let i = 0; i < slides.length; i++) {
+
+		let bulletPoints = document.createElement("div");
+		bulletPoints.textContent=".";
+		bulletPoints.classList.add("dot");
+		const dotsDiv = document.querySelector(".dots")
+		dotsDiv.appendChild(bulletPoints);
+		}
+}
+
+createBulletPoints ()
+
